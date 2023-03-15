fd9ecb1e912dd62ae5ae22f18c73bbc26c7c4938
dnr1112 <dnr1112@naver.com>
2023-03-15 10:55:44 +0900

    23.03.15 수업

---
+++ javascript/js/window.js
@@ -0,0 +1,9 @@
+const naverButton = document.getElementsByTagName("button");
+naverButton[0].onclick = () => {
+    // location.href = "http://www.naver.com";
+    location.replace("http://www.naver.com");
+}
+
+naverButton[1].onclick = () => {
+    location.href = "file:///C:/junil/changwook/workspace/web_basic/-AWS-_web_study/javascript/js-html/array.html"
+}
